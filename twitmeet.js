5203f592cdb20bd71ed9b26126b7037ba80d2cb2
samikshaparkhe <86120216+samikshaparkhe@users.noreply.github.com>
2021-11-09 21:32:52 +0530

    Add files via upload

---
+++ twitmeet.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD9pSrXltuizb6K3Qb7RhSilwqkx-yGPH8",
+    authDomain: "twitmeet-c-94.firebaseapp.com",
+    projectId: "twitmeet-c-94",
+    storageBucket: "twitmeet-c-94.appspot.com",
+    messagingSenderId: "631828689538",
+    appId: "1:631828689538:web:27148b5a30d2d4f444fa9b"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
